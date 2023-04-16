e77b2df52b1593f3d8681bdc8bbdce14532b47f6
Martin White <martinjameswhite@users.noreply.github.com>
2023-04-16 08:54:57 -0700

    Handle weights properly.

--- mocks/calc_wR.py
+++ mocks/calc_wR.py
@@ -1,77 +1,56 @@
 #!/usr/bin/env python3
 #
-# Compute the angular correlation function of a
-# catalog of objects.
+# Uses Corrfunc to compute the angular correlation function
+# of a sample (provided data and randoms).
+#
 #
 import numpy as np
 import os
-#
-from astropy.table import Table
-#
-from Corrfunc.mocks import DDtheta_mocks as Pairs
-from Corrfunc.utils import convert_3d_counts_to_cf
+
+from Corrfunc.mocks.DDtheta_mocks import DDtheta_mocks
+
+
 
 
 
 def calc_wt(dat,ran,bins=None):
-    """Does the work of calling CorrFunc."""
+    """Computes the (L-S) angular correlation function"""
     # Get number of threads, datas and randoms.
-    nthreads = int( os.getenv('OMP_NUM_THREADS','1') )
-    Nd,Nr    = len(dat['RA']),len(ran['RA'])
-    # RA and DEC should be in degrees, and all arrays should
-    # be the same type and it seems as if they need to be
-    # 'float' and not e.g. 'float32'.  Ensure this now.
-    pp  = 'pair_product'
-    dra = dat['RA' ].astype('float')
-    ddc = dat['DEC'].astype('float')
-    if not 'WT' in dat.keys():
-        dwt = np.ones_like(dat['RA']).astype('float')
-    else:
-        dwt = dat['WT'].astype('float')
-    rra = ran['RA' ].astype('float')
-    rdc = ran['DEC'].astype('float')
-    if not 'WT' in ran.keys():
-        rwt = np.ones_like(ran['RA']).astype('float')
-    else:
-        rwt = ran['WT'].astype('float')
-    # Bin edges are specified in degrees, if nothing
+    nt   = int(os.getenv('OMP_NUM_THREADS',1))
+    pp   = 'pair_product'
+    # Bin edges are specified in Mpc/h, if nothing
     # is passed in, do log-spaced bins.
     if bins is None:
-        Nbin = 8
-        bins = np.geomspace(0.01,0.3,Nbin+1)
-    # do the pair counting, then convert to w(theta).
-    DD = Pairs(1,nthreads,bins,RA1=dra,DEC1=ddc,weights1=dwt,weight_type=pp)
-    RR = Pairs(1,nthreads,bins,RA1=rra,DEC1=rdc,weights1=rwt,weight_type=pp)
-    DR = Pairs(0,nthreads,bins,RA1=dra,DEC1=ddc,RA2=rra,DEC2=rdc,\
-               weights1=dwt,weights2=rwt,weight_type=pp)
-    wt = convert_3d_counts_to_cf(Nd,Nd,Nr,Nr,DD,DR,DR,RR)
-    # Return the binning and w(theta).
-    return( (bins,wt) )
-    #
-
-
-if __name__=="__main__":
-    rng  = np.random.default_rng(1)
-    # Load the data from file.
-    dat  = Table.read('mock_lae_cat.fits')
-    chi0 = dat.meta['CHI0']
-    Lside= dat.meta['LSIDE']
-    Lx   = Lside/chi0 * 180./np.pi
-    Ly   = Lside/chi0 * 180./np.pi
-    # Generate a uniform random distribution.
-    nran = 100000
-    ran  = {}
-    ran['RA' ] = rng.uniform(low=-Lx/2.,high=Lx/2.,size=nran)
-    ran['DEC'] = rng.uniform(low=-Ly/2.,high=Ly/2.,size=nran)
-    # Get rid of negative RAs, just for ease.
-    dat['RA'] += Lx
-    ran['RA'] += Lx
-    # and compute w(theta), converting theta to projected
-    # distance, R, assuming log-space bins.
-    bins,wt = calc_wt(dat,ran)
-    rval    = chi0*np.sqrt(bins[:-1]*bins[1:])*np.pi/180.
-    # Print the results.
-    print("# {:>8s} {:>15s}".format("R[Mpc/h]","w_theta(R)"))
-    for i in range(wt.size):
-        print("{:10.4f} {:15.5e}".format(rval[i],wt[i]))
+        Nbin = 10
+        bb   = np.geomspace(0.005,0.5,Nbin+1)
+    else:
+        bb   = bins
+    # RA and DEC should be in degrees, and all arrays should
+    # be the same type, and it seems they need to be float
+    # and not e.g. float32.  Ensure this now.
+    dra = np.ascontiguousarray(dat['RA' ]).astype('float')
+    ddc = np.ascontiguousarray(dat['DEC']).astype('float')
+    rra = np.ascontiguousarray(ran['RA' ]).astype('float')
+    rdc = np.ascontiguousarray(ran['DEC']).astype('float')
+    if 'WT' in dat.keys():
+        dwt = np.ascontiguousarray(dat['WT']).astype('float')
+    else:
+        dwt = np.ones_like(dat['RA']).astype('float')
+    if 'WT' in ran.keys():
+        rwt = np.ascontiguousarray(ran['WT']).astype('float')
+    else:
+        rwt = np.ones_like(ran['RA']).astype('float')
+    # Get the weight sums and the pair counts.
+    nd,nr= np.sum(dwt),np.sum(rwt)
+    DD_c = DDtheta_mocks(1,nt,bb,RA1=dra,DEC1=ddc,weights1=dwt,weight_type=pp)
+    RR_c = DDtheta_mocks(1,nt,bb,RA1=rra,DEC1=rdc,weights1=rwt,weight_type=pp)
+    DR_c = DDtheta_mocks(0,nt,bb,RA1=dra,DEC1=ddc,weights1=dwt,\
+                                 RA2=rra,DEC2=rdc,weights2=rwt,weight_type=pp)
+    # Compute the normalized, weighted pair counts.
+    dd   = DD_c['npairs']*DD_c['weightavg']/nd/nd
+    dr   = DR_c['npairs']*DR_c['weightavg']/nd/nr
+    rr   = RR_c['npairs']*RR_c['weightavg']/nr/nr
+    # and hence the angular correlation function.
+    wt   = (dd-2*dr+rr)/(rr+1e-30)
+    return((bins,wt))
     #
