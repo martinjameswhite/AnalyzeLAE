7b165c23696a38d9b5b71e2f181668663d4197b4
Martin White <martinjameswhite@users.noreply.github.com>
2023-05-15 08:48:03 -0700

    ZCV options.

--- mocks/lae_clustering.py
+++ mocks/lae_clustering.py
@@ -11,7 +11,8 @@ from abacusnbody.metadata       import get_meta
 # These are the keys we will copy from the simulation meta-data into
 # the output JSON file.
 simkeys = ['n_s', 'omega_b', 'omega_cdm', 'omega_ncdm', 'N_ncdm', 'N_ur',\
-           'H0', 'w0', 'wa', 'w', 'Omega_DE', 'Omega_K', 'Omega_M', 'Omega_Smooth', \
+           'H0', 'w0', 'wa', 'w',\
+           'Omega_DE', 'Omega_K', 'Omega_M', 'Omega_Smooth', \
            'Redshift', 'ScaleFactor', \
            'OmegaNow_DE', 'OmegaNow_K', 'OmegaNow_m', \
            'f_growth', 'fsmooth', 'Growth', 'Growth_on_a_n', \
@@ -34,7 +35,8 @@ want_rsd      = HOD_params['want_rsd']
 write_to_disk = HOD_params['write_to_disk']
 want_zcv      = False
 #
-# Load the rp pi binning from the config file. Note pimax and pi_bin_size are ints.
+# Load the rp pi binning from the config file.
+# Note pimax and pi_bin_size are ints.
 bin_params = clustering_params['bin_params']
 rpbins     = np.logspace(bin_params['logmin'],\
                          bin_params['logmax'],bin_params['nbins']+1)
@@ -49,12 +51,14 @@ Zcen = np.arange(0.0,float(pimax),dpi) + 0.5*dpi
 # Satellite numbers are ncen times ([M-kappa.Mcut]/M1)^alpha
 #
 if want_rsd:
-    lgMc_list = [11.50,11.75,12.00,12.25]
+    lgMc_list = [11.00,11.25,11.50,11.75,12.00]
+    lgMc_list = [10.9,11.0,11.1,11.2,11.3,11.4,11.5,11.6,11.7,11.8,11.9]
     alph_list = [0.33,0.50,0.66]
     sigm_list = [0.50,0.66]
     plat_list = [5.,10.]
 else:
-    lgMc_list = [11.50,11.75,12.00,12.25]
+    lgMc_list = [11.00,11.25,11.50,11.75,12.00]
+    lgMc_list = [10.9,11.0,11.1,11.2,11.3,11.4,11.5,11.6,11.7,11.8,11.9]
     alph_list = [0.33,0.50,0.66]
     sigm_list = [0.50,0.66]
     plat_list = [5.,10.]
@@ -82,36 +86,43 @@ for lgMcut in lgMc_list:
         if want_zcv:
             # Compute variance reduced spectra.
             zcv_dict = newBall.apply_zcv(mock_dict,config)
+            bb = zcv_dict['b1'] + 1.0 # Convert to Eulerian bias.
             kk = zcv_dict['k_binc']
             pkl= zcv_dict['Pk_tr_tr_ell_zcv'] # variance-reduced multipoles
             pk0= pkl[0]
             pk2= pkl[1]
-        else:
-            # Just something to put in the file.
-            kk,pk0,pk2 = np.zeros(1),np.zeros(1),np.zeros(1)
-        #
-        if False: # Old code.
+        else: # Old code.
             pk3d = newBall.compute_power(mock_dict,nbins_k=50,nbins_mu=11,\
-                      k_hMpc_max=0.5,logk=False,poles=[0,2],num_cells=512,\
-                      paste='TSC',compensated=True,interlaced=True)
+                     k_hMpc_max=0.5,logk=False,poles=[0,2],num_cells=512,\
+                     paste='TSC',compensated=True,interlaced=True)
             kk   = pk3d['k_binc']
             pkl  = pk3d['LRG_LRG_ell']
             pk0  = pkl[0,:,0]
             pk2  = pkl[1,:,0]
+            bb   = 0.0 # Placeholder.
         #
         if nobj>maxobj:
-            print("Have nobj=",nobj," downsampling to ",maxobj)
+            print("Have nobj=",nobj," downsampling to ",maxobj,flush=True)
             rng  = np.random.default_rng()
             inds = rng.choice(nobj,size=maxobj,replace=False)
             for k in ['x','y','z','vx','vy','vz','mass','id']:
                 mock_dict['LRG'][k] = mock_dict['LRG'][k][inds]
-        #wpR   = newBall.compute_wp(mock_dict,rpbins,pimax,dpi)['LRG_LRG']
-        xiell = newBall.compute_multipole(mock_dict,rpbins,pimax,dpi)['LRG_LRG']
-        wpR   = xiell[0*len(Rcen):1*len(Rcen)]
-        xi0   = xiell[1*len(Rcen):2*len(Rcen)]
-        xi2   = xiell[2*len(Rcen):3*len(Rcen)]
+        if want_zcv:
+            zcv_dict = apply_zcv_xi(mock_dict,config,load_presaved=False)
+            bb       = zcv_dict['b1'] + 1.0 # Convert to Eulerian bias.
+            xiell    = zcv_dict['Xi_tr_tr_ell_zcv']
+            xi0      = xiell[0]
+            xi2      = xiell[1]
+        else: # Old code.
+            #wpR   = newBall.compute_wp(mock_dict,rpbins,pimax,dpi)['LRG_LRG']
+            xiell = newBall.compute_multipole(mock_dict,rpbins,pimax,dpi)
+            xiell = xiell['LRG_LRG']
+            wpR   = xiell[0*len(Rcen):1*len(Rcen)]
+            xi0   = xiell[1*len(Rcen):2*len(Rcen)]
+            xi2   = xiell[2*len(Rcen):3*len(Rcen)]
+            bb    = 0.0 # Placeholder.
         #
-        dats.append({'hod':hod,'nobj':nobj,'fsat':fsat,\
+        dats.append({'hod':hod,'nobj':nobj,'fsat':fsat,'bias':bb,\
                      'wp':wpR.tolist(),'xi0':xi0.tolist(),'xi2':xi2.tolist(),\
                      'pk0':pk0.tolist(),'pk2':pk2.tolist()})
 # Now write out our answers.
