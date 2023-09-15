a1f92c5ac358b7007ff277c5ab4b3468194ccbb8
Martin White <martinjameswhite@users.noreply.github.com>
2023-09-15 11:00:25 -0700

    Fiducial numbers.

---
+++ odin/fiducial.py
@@ -0,0 +1,18 @@
+# Contains "fiducial" distances and interloper fractions for
+# the various sources.  This way all scripts can import this
+# information without the need to synchronize.
+#
+# The distances are from make_table while the interloper
+# fractions are from VI, assuming redshift failures have
+# the same z-distribution as non-failures.
+
+# Comoving distance to field center.  Used to convert angles
+# to distances.
+chi_dict = {}
+chi_dict['N419'] = 3941.0
+chi_dict['N501'] = 4448.0
+chi_dict['N673'] = 5160.0
+
+# Interloper fractions.
+fint_dict = {}
+fint_dict['N501'] = {'s0':0.094,'s1':0.025,'s2':0.023,'s3':0.032}
