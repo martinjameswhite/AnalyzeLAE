3fa9e8803a8b60f70da50e27e7c39fff10a3a3ec
Martin White <martinjameswhite@users.noreply.github.com>
2023-10-26 13:57:43 -0700

    Compute clustering script.

---
+++ odin/compute_clustering.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH -J Wt
+#SBATCH -N 1
+#SBATCH -t 00:10:00
+#SBATCH -o Compute_clustering.out
+#SBATCH -e Compute_clustering.err
+#SBATCH -q debug
+#SBATCH -C cpu
+#SBATCH -A m68
+#
+date
+#
+module use /global/common/software/nersc/user/gsl-temp-modulefile
+module load gsl
+source activate abacus
+#
+export OMP_NUM_THREADS=64
+srun -n 1 -c ${OMP_NUM_THREADS} python compute_clustering.py
+#
+chmod og+r *
+date
+#
