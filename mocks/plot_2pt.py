2803f7cb28912414c980a63af76f949178c999ce
Martin White <martinjameswhite@users.noreply.github.com>
2023-11-21 09:13:01 -0800

    Pass filter name and zeff.

--- mocks/plot_2pt.py
+++ mocks/plot_2pt.py
@@ -8,10 +8,10 @@ import matplotlib.pyplot as plt
 
 
 
-def make_xir_plot():
+def make_xir_plot(filt_name,zeff):
     """Does the work."""
     # Load the data.
-    br   = np.loadtxt("lae_br.txt")
+    br   = np.loadtxt("lae_"+filt_name+"_br.txt")
     ximm = br[:,3]
     xigg = br[:,1]**2 * ximm
     xigm = br[:,2]    * ximm
@@ -21,7 +21,8 @@ def make_xir_plot():
     ax.plot(br[:,0],xigg,'d',color='C0',mfc='None',label=r'$\xi_{gg}$')
     ax.plot(br[:,0],xigm,'s',color='C1',mfc='None',label=r'$\xi_{gm}$')
     ax.plot(br[:,0],ximm,'o',color='C2',mfc='None',label=r'$\xi_{mm}$')
-    ax.legend(title=r'N501, $z\simeq 3.1$',loc=1)
+    tstring = filt_name.upper()+r', $z\simeq '+'{:.1f}$'.format(zeff)
+    ax.legend(title=tstring,loc=1)
     ax.set_xlim(0.5,25)
     ax.set_ylim(1e-2,25)
     ax.set_xscale('log')
@@ -30,17 +31,17 @@ def make_xir_plot():
     ax.set_ylabel(r'$\xi(r)$')
     #
     plt.tight_layout()
-    plt.savefig('odin_N501_xir.png')
-    plt.savefig('odin_N501_xir.pdf')
+    plt.savefig('odin_'+filt_name.upper()+'_xir.png')
+    plt.savefig('odin_'+filt_name.upper()+'_xir.pdf')
     #
 
 
 
 
-def make_pkr_plot():
+def make_pkr_plot(filt_name,zeff):
     """Does the work."""
     # Load the data.
-    bk  = np.loadtxt("lae_bk.txt")
+    bk  = np.loadtxt("lae_"+filt_name+"_bk.txt")
     Pmm = bk[:,3]
     Pgg = bk[:,1]**2 * Pmm
     Pgm = bk[:,2]    * Pmm
@@ -50,7 +51,8 @@ def make_pkr_plot():
     ax.plot(bk[:,0],Pgg,'d',color='C0',mfc='None',label=r'$P_{gg}$')
     ax.plot(bk[:,0],Pgm,'s',color='C1',mfc='None',label=r'$P_{gm}$')
     ax.plot(bk[:,0],Pmm,'o',color='C2',mfc='None',label=r'$P_{mm}$')
-    ax.legend(title=r'N501, $z\simeq 3.1$',loc=1)
+    tstring = filt_name.upper()+r', $z\simeq '+'{:.1f}$'.format(zeff)
+    ax.legend(title=tstring,loc=1)
     ax.set_xlim(0.0,0.5)
     ax.set_ylim(20,2e4)
     ax.set_yscale('log')
@@ -58,13 +60,14 @@ def make_pkr_plot():
     ax.set_ylabel(r'$P(k)\quad [h^{-3}{\rm Mpc}^3]$')
     #
     plt.tight_layout()
-    plt.savefig('odin_N501_pkr.png')
-    plt.savefig('odin_N501_pkr.pdf')
+    plt.savefig('odin_'+filt_name.upper()+'_pkr.png')
+    plt.savefig('odin_'+filt_name.upper()+'_pkr.pdf')
     #
 
 
 
 
 if __name__=="__main__":
-    make_xir_plot()
-    make_pkr_plot()
+    for filt_name,zeff in zip(["n501"],[3.1]):
+        make_xir_plot(filt_name,zeff)
+        make_pkr_plot(filt_name,zeff)
